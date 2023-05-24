920315a800c0025eb3e631db48c90c185cc326b8
Shekhar Mehta <shekharmehta25@gmail.com>
2023-05-23 21:55:16 -0400

    2nd commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { auth } from './firebase';
 import Navbar from './NavBar';
 import HomePage from './HomePage';
 import Login from './Login';
 import Signup from './Signup';
+import './App.css';
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null)
+        console.log("logout successful");
+      }
+    });
+
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  }, []);
+
   return (
     <Router>
       <div className="App">
-        <Navbar />
+        <Navbar user={user} />
         <div className="content">
           <Switch>
             <Route exact path="/">
-              <HomePage />
+              {user ? <HomePage /> : <Redirect to="/login" />}
             </Route>
             <Route path="/login">
-              <Login />
+              {user ? <Redirect to="/" /> : <Login />}
             </Route>
             <Route path="/signup">
-              <Signup />
+              {user ? <Redirect to="/" /> : <Signup />}
             </Route>
           </Switch>
         </div>
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,14 +1,24 @@
-const Navbar = () => {
-    return ( 
-        <nav className="navbar">
-            <h1>CharityChain</h1>
-            <div className="links">
+import { auth } from './firebase';
+
+const Navbar = ({ user }) => {
+  const handleLogout = () => {
+    auth.signOut();
+  };
+
+  return ( 
+    <nav className="navbar">
+        <h1>CharityChain</h1>
+        <div className="links">
+            { user ? <button onClick={handleLogout}>Logout</button> : (
+              <>
                 <a href="/">Home</a>
                 <a href="/login">Login</a>
                 <a href="/signup">Sign Up</a>
-            </div>
-        </nav>
-     );
+              </>
+            )}
+        </div>
+    </nav>
+  );
 }
- 
+
 export default Navbar;
